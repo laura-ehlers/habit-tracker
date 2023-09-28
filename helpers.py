bf804c50599a1d5dbde7b05cb8d19e53dccb5f61
Laura Ehlers <laura@ehlers-mail.com>
2023-09-28 12:14:32 +0200

    done symbol is shown correctly, check habit works

--- helpers.py
+++ helpers.py
@@ -3,7 +3,7 @@ from rich.console import Console
 from rich.table import Table
 from habit import Habit
 import questionary
-import datetime
+import datetime as dt
 from database import (
     get_all_habits,
     delete_habit_from_db,
@@ -23,20 +23,32 @@ def increment_streak(position: int):
 
     cur_habit = get_habit_by_position(position)
 
-    cur_date = datetime.date.today()
-    if cur_habit.last_date_checked == cur_date:
-        return
-    elif cur_habit.last_date_checked - cur_date > 1:
-        cur_habit.last_date_checked = cur_date
-        cur_habit.streak = 0
-    elif (
-        cur_habit.last_date_checked - cur_date == 1
-        or cur_habit.last_date_checked is None
-    ):
-        cur_habit.last_date_checked = cur_date
-        cur_habit.streak += 1
+    # cur_date = date.today()
+    cur_date = dt.datetime.combine(dt.date.today(), dt.datetime.min.time())
+
+    # print(cur_habit[2])
+    if cur_habit["last_date_checked"] is None:
+        cur_habit["last_date_checked"]= cur_date
+        cur_habit["streak"] += 1
+
     else:
-        print("Error: there is a problem with the last checked date.")
+        date_cur_habit = dt.datetime.strptime(
+            cur_habit["last_date_checked"], "%Y-%m-%d"
+        )
+
+        if date_cur_habit == cur_date:
+            return
+        elif date_cur_habit - cur_date > dt.timedelta(days=1):
+            date_cur_habit = cur_date
+            cur_habit["streak"]  = 0
+            cur_habit["last_date_checked"] = date_cur_habit
+        elif date_cur_habit - cur_date == dt.timedelta(days=1) or date_cur_habit is None:
+            cur_habit["last_date_checked"] = date_cur_habit
+            cur_habit["streak"]  += 1
+        else:
+            print("Error: there is a problem with the last checked date.")
+    
+    return cur_habit
 
 
 def write_csv():
