e0f9cc7adefe762bb227b8ef8dd0a0ebff6b0746
Laura Ehlers <laura@ehlers-mail.com>
2023-10-05 23:07:59 +0200

    fixed calculation for daily streak calculation

--- helpers.py
+++ helpers.py
@@ -4,9 +4,7 @@ from rich.table import Table
 from habit import Habit
 import questionary
 import datetime as dt
-from database import (
-    get_habit_by_position,
-)
+from database import get_habit_by_position, calculate_streak
 import csv
 
 
@@ -15,35 +13,47 @@ def increment_streak(position: int):
     - increment streak everyday by 1
     - if habit is checked and then unchecked decrease by 1 again
     """
+
     cur_habit = get_habit_by_position(position)
+    print(cur_habit)
 
     # cur_date = date.today()
-    datetime_today = dt.datetime.now().replace(second= 0, microsecond = 0)
+    datetime_today = dt.datetime.now().replace(second=0, microsecond=0)
     today = dt.datetime.now().date()
-    
+
     # print(cur_habit[2])
     if cur_habit["last_date_checked"] is None:
-        cur_habit["last_date_checked"]= today
-        cur_habit["streak"] += 1
-
+        cur_habit["last_date_checked"] = today
+        cur_habit["streak"] = 1
     else:
         datetime_cur_habit = dt.datetime.strptime(
-            cur_habit["last_date_checked"], "%Y-%m-%d %H:%M:%S")
+            cur_habit["last_date_checked"], "%Y-%m-%d %H:%M:%S"
+        )
+        
         date_cur_habit = datetime_cur_habit.date()
         print(datetime_cur_habit)
 
         if date_cur_habit == today:
-            return datetime_today
-        elif date_cur_habit - today > dt.timedelta(days=1):
+            # cur_habit["streak"] = 1
+            # cur_habit["last_date_checked"] = datetime_today
+            print("if")
+            return 
+            
+        elif  today - date_cur_habit > dt.timedelta(days=1):
             datetime_cur_habit = datetime_today
-            cur_habit["streak"]  = 0
+            cur_habit["streak"] = 1
             cur_habit["last_date_checked"] = datetime_cur_habit
-        elif date_cur_habit - today == dt.timedelta(days=1) or date_cur_habit is None:
+            print("elif 1")
+        elif today - date_cur_habit == dt.timedelta(days=1) or date_cur_habit is None:
             cur_habit["last_date_checked"] = datetime_cur_habit
-            cur_habit["streak"]  += 1
+            cur_habit["streak"] += 1
+            print("elif 2")
         else:
             print("Error: there is a problem with the last checked date.")
-    
+            return
+
+    calculate_streak(cur_habit["position"], cur_habit["streak"])
+
     return cur_habit
 
 
@@ -63,14 +73,46 @@ def write_csv():
             ]
         )
         writer.writerow(
-            ["Make the bed", "Work", "Weekly", "2023-07-10 11:10:00", "2023-08-21 15:10:00", "3", 2]
+            [
+                "Make the bed",
+                "Work",
+                "Weekly",
+                "2023-07-10 11:10:00",
+                "2023-08-21 15:10:00",
+                "3",
+                2,
+            ]
         )
         writer.writerow(
-            ["Cuddle dog", "Work", "Weekly", "2023-07-10 19:10:00", "2023-08-21 12:10:00", "3", 2]
+            [
+                "Cuddle dog",
+                "Work",
+                "Weekly",
+                "2023-07-10 19:10:00",
+                "2023-08-21 12:10:00",
+                "3",
+                2,
+            ]
         )
         writer.writerow(
-            ["Cuddle dog", "Work", "Weekly", "2023-07-10 17:10:00", "2023-08-21 17:10:00", "3", 2]
+            [
+                "Cuddle dog",
+                "Work",
+                "Weekly",
+                "2023-07-10 17:10:00",
+                "2023-08-21 17:10:00",
+                "3",
+                2,
+            ]
         )
         writer.writerow(
-            ["Cuddle dog", "Work", "Weekly", "2023-07-10 13:10:00", "2023-09-10 11:10:00", "3", 1]
+            [
+                "Cuddle dog",
+                "Work",
+                "Weekly",
+                "2023-07-10 13:10:00",
+                "2023-10-04 11:10:00",
+                "3",
+                1,
+            ]
         )
